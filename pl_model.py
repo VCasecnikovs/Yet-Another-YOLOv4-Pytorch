341d6a606cd57ef858bb0f6e747c82ec53b27223
Vadims Chashechnikovs <casecnikov@gmail.com>
2020-07-17 22:59:21 +0300

    Added SAT using fgsm, also (Batch + LARS checked)

--- pl_model.py
+++ pl_model.py
@@ -30,13 +30,36 @@ class YOLOv4PL(pl.LightningModule):
     def forward(self, x, y=None):
         return self.model(x, y)
 
-    def training_step(self, batch, batch_idx):
+    def basic_training_step(self, batch):
         filenames, images, labels = batch
         y_hat, loss = self(images, labels)
 
         logger_logs = {"training_loss" : loss}
 
         return {"loss" : loss, "log" : logger_logs}
+
+    def sat_training_step(self, batch, epsilon = 0.01):
+        filenames, images, labels = batch
+
+        images.requires_grad_(True)
+        y_hat, loss = self(images, labels)
+        loss.backward()
+        data_grad = images.grad.data
+        images.requires_grad_(False)
+        images = torch.clamp(images + data_grad.sign() * epsilon, 0, 1)
+
+        return self.basic_training_step((filenames, images, labels))
+
+        
+
+
+    def training_step(self, batch, batch_idx):
+        if hparams.SAT:
+            pass
+        else:
+            return self.basic_training_step(batch)
+        
+        
 
     def training_epoch_end(self, outputs):
         training_loss_mean = torch.stack([x['training_loss'] for x in outputs]).mean()
@@ -60,7 +83,7 @@ class YOLOv4PL(pl.LightningModule):
         if self.hparams.optimizer == "SGD":
             self.optimizer = torch.optim.SGD(params, self.hparams.lr, momentum = self.hparams.momentum, weight_decay=self.hparams.wd)
         elif self.hparams.optimizer == "LARS":
-            self.optimizer = LARS(params, lr=self.hparams.lr, momentum=self.hparams.momentum, weight_decay=self.hparams.wd)
+            self.optimizer = LARS(params, lr=self.hparams.lr, momentum=self.hparams.momentum, weight_decay=self.hparams.wd, max_epoch=self.hparams.epochs)
 
         self.scheduler = torch.optim.lr_scheduler.OneCycleLR(self.optimizer, self.hparams.lr, epochs=self.hparams.epochs, steps_per_epoch=1, pct_start=self.hparams.pct_start)
         sched_dict = {'scheduler': self.scheduler}
