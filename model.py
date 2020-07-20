32e619b528650c62ca0c9f0d7f0550ce26906b24
Vadims Chashechnikovs <casecnikov@gmail.com>
2020-07-21 00:04:22 +0300

    fixes weight standartization bug

--- model.py
+++ model.py
@@ -117,9 +117,10 @@ class ECA(nn.Module):
 
 #Taken from https://github.com/joe-siyuan-qiao/WeightStandardization
 class Conv2dWS(nn.Conv2d):
-    def __init__(self, *args, **kwargs):
-                 
-        super(nn.Conv2d, self).__init__(*args, **kwargs)
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1,
+                 padding=0, dilation=1, groups=1, bias=True):
+        super(Conv2dWS, self).__init__(in_channels, out_channels, kernel_size, stride,
+                 padding, dilation, groups, bias)
 
     def forward(self, x):
         weight = self.weight
